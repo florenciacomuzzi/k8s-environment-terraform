23d571706c49bb72b016f9a20726a07727e49a77
Florencia Comuzzi <florenciacomuzzi@gmail.com>
2025-03-06 15:04:34 -0500

    enable compute engine api

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "compute_engine" {
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_compute_network" "vpc_network" {
   name = var.vpc_name
 }
